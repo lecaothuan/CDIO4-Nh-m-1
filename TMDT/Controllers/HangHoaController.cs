67f49bf1ec08bed663816a868f09a40f84be6323
lecaothuan <thuanlecao@gmail.com>
2024-06-17 15:28:54 +0700

    #

---
+++ TMDT/Controllers/HangHoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TMDT.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
