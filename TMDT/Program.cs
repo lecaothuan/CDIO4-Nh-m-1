0317c59eff5622062e6026b58748c8d9e303d8a0
lecaothuan <thuanlecao@gmail.com>
2024-06-17 14:17:24 +0700

    #

--- TMDT/Program.cs
+++ TMDT/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TMDT.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<TmdtContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TMDT"));
+});
 
 var app = builder.Build();
 
