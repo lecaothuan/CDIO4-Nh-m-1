fbb0bb535f9528ac6f2dac2ae1eaecca2c03d170
lecaothuan <thuanlecao@gmail.com>
2024-06-17 22:30:17 +0700

    menu

---
+++ TMDT/ViewComponents/MenuLoaiViewComponents.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TMDT.Data;
+using TMDT.ViewModels;
+namespace TMDT.ViewComponents
+{
+    public class MenuLoaiViewComponents : ViewComponent
+    {
+        private readonly TmdtContext db;
+
+        public MenuLoaiViewComponents(TmdtContext context) => db = context; 
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Loais.Select(lo => new MenuLoaiVM
+            {
+                MaLoai = lo.MaLoai, TenLoai = lo.TenLoai, SoLuong = lo.HangHoas.Count
+            });
+            return View(data);
+        }
+    }
+}
