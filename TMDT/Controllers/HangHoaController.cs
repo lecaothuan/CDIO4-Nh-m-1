b4b9dfae87ae588e513a243b89c276025379407a
lecaothuan <thuanlecao@gmail.com>
2024-06-20 15:30:52 +0700

    chi tiết hàng hóa

--- TMDT/Controllers/HangHoaController.cs
+++ TMDT/Controllers/HangHoaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TMDT.Data;
 using TMDT.ViewModels;
 
@@ -8,7 +9,8 @@ namespace TMDT.Controllers
     {
         private readonly TmdtContext db;
 
-        public HangHoaController(TmdtContext context) {
+        public HangHoaController(TmdtContext context)
+        {
             db = context;
         }
         public IActionResult Index(int? loai)
@@ -49,6 +51,34 @@ namespace TMDT.Controllers
                 MoTaNgan = P.MoTaDonVi ?? "",
                 TenLoai = P.MaLoaiNavigation.TenLoai
             });
+            return View(result);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var data = db.HangHoas
+                .Include(p => p.MaLoaiNavigation)
+                .SingleOrDefault(p => p.MaHh == id);
+            if (data == null)
+            {
+                TempData["Message"] = $"Không thấy sãn phẩm có mã {id}";
+                return Redirect("/404");
+            }
+            var result = new ChiTietHangHoaVM
+            {
+                MaHH = data.MaHh,
+                TenHH = data.TenHh,
+                DonGia = data.DonGia ?? 0,
+                ChiTiet = data.MoTa ?? string.Empty,
+                Hinh = data.Hinh ?? string.Empty,
+                MoTaNgan = data.MoTaDonVi ?? string.Empty,
+                TenLoai = data.MaLoaiNavigation.TenLoai,
+                SoLuongTon = 10,
+                DiemDanhGia = 5,
+
+            };
+
+
             return View(result);
         }
     }
