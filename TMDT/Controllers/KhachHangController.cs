f6a2223a2da4938d4582766b0c75a48baea79571
lecaothuan <thuanlecao@gmail.com>
2024-07-01 11:20:07 +0700

    dang ky

---
+++ TMDT/Controllers/KhachHangController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TMDT.Data;
+using TMDT.ViewModels;
+
+namespace TMDT.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly TmdtContext db;
+        public KhachHangController(TmdtContext context) 
+        { 
+            db=context;
+        }
+        [HttpGet]
+        public IActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DangKy(RegisterVM model)
+        {
+            if (ModelState.IsValid) 
+            {
+                var khachHang = model;
+            }
+            return View();
+        }
+    }
+}
